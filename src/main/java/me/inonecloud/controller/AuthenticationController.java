be90ce944381dc69c2f47e689f89aeae9c5b36fe
Andrew <andrewyelmanov@gmail.com>
2020-03-14 16:03:00 +0300

    Opened cors for 3000 port

--- src/main/java/me/inonecloud/controller/AuthenticationController.java
+++ src/main/java/me/inonecloud/controller/AuthenticationController.java
@@ -9,14 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 @RequestMapping("api")
 public class AuthenticationController {
